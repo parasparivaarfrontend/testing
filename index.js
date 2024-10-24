2f378914a6b77d28de97ebccc680edba4c4bfa87
jhahimanshu498@gmail.com <jhahimanshu498@gmail.com>
2024-10-24 15:02:48 +0530

    change 2

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ app.listen(4000,()=>{
 
 app.get("/test",(_req,res)=>{
     res.cookie("token", "test", {
-        secure: true, expires: new Date(Date.now() + 1000 * 60 * 2), sameSite: "lax", maxAge: 1000 * 60 * 2, signed:true })
+        secure: true, expires: new Date(Date.now() + 1000 * 60 * 2), sameSite: "lax", maxAge: 1000 * 60 * 2 })
     res.send("done")
 })
