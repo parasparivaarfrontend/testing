107d923cf7cac5597e7fa5103eac42bb572ac5e2
jhahimanshu498@gmail.com <jhahimanshu498@gmail.com>
2024-10-24 16:56:44 +0530

    some change

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(json({limit:"10mb"}))
 app.use(urlencoded({extended:true,limit:"10mb"}))
-app.use(cors({ origin:"http://localhost:3000",credentials:true}))
+app.use(cors({ origin:"https://chipper-cassata-7e72ee.netlify.app",credentials:true}))
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
@@ -24,6 +24,6 @@ app.listen(4000,()=>{
 
 app.get("/test",(_req,res)=>{
     res.cookie("token", "test", {
-        secure: true, expires: new Date(Date.now() + 1000 * 60 * 2), sameSite: "lax", maxAge: 1000 * 60 * 2 })
+        secure: true, expires: new Date(Date.now() + 1000 * 60 * 2), sameSite: "none", maxAge: 1000 * 60 * 2 })
     res.send("done")
 })
