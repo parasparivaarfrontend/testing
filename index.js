57346559f6f6e933634420f57d638174e2562103
jhahimanshu498@gmail.com <jhahimanshu498@gmail.com>
2024-10-24 12:48:21 +0530

    again test 2

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ app.use(cors({ origin:"http://localhost:3000",credentials:true}))
 
 
 app.listen(4000,()=>{
-    console.log("start server on port 3000")
+    console.log("start server on port 4000")
 })
 
 
 app.get("/test",(_req,res)=>{
     res.cookie("token", "test", {
-        secure: true, expires: new Date(Date.now() + 1000 * 60 * 60), sameSite: "None", domain: 'http://localhost:3000' })
+        secure: true, expires: new Date(Date.now() + 1000 * 60 * 60), sameSite: "None", domain:"localhost" })
     res.send("done")
 })
